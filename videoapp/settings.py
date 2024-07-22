e05d7a35c8fcef2757058e19ee088b35d7106350
prateekTut <126943406+prateekTut@users.noreply.github.com>
2024-07-22 16:40:02 +0530

    Update settings.py

--- videoapp/settings.py
+++ videoapp/settings.py
@@ -73,7 +73,15 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'videoapp.wsgi.application'
+CORS_ALLOWED_ORIGINS = [
+    "https://appforstreaming123.azurewebsites.net",
+]
 
+CSRF_TRUSTED_ORIGINS = ['https://appforstreaming123.azurewebsites.net']
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://appforstreaming123.azurewebsites.net",
+]
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
